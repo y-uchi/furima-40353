946f2dba8919c1f3998485086a5cc2cc558d9b97
y-uchi <bonbonheart@gmail.com>
2024-05-05 17:55:07 +0900

    ユーザー新規登録及/ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' } 
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :kana_family_name, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :kana_first_name, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :birthday
+    end
 end
