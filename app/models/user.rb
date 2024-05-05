c0755189c76449c9157cabc14bf14e57fde0646d
y-uchi <bonbonheart@gmail.com>
2024-05-05 19:56:35 +0900

    userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' } 
+  validates :password, format: { with: VALID_PASSWORD_REGEX } 
 
   with_options presence: true do
     validates :nickname
