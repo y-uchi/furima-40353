ac253e5a2964536fd961053436e072b0c4c26e2c
y-uchi <bonbonheart@gmail.com>
2024-05-29 20:29:19 +0900

    挙動確認後の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('turbo:load', () =>{
+const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
@@ -7,4 +7,7 @@ window.addEventListener('turbo:load', () =>{
     const addProfitDom = document.getElementById("profit");
     addProfitDom.innerHTML = Math.floor(priceInput.value - priceInput.value * 0.1)
   })
-})
+}
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
