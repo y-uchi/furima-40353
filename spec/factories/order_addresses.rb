4ac2e60851be11dace3be562316b2fe131527c92
y-uchi <bonbonheart@gmail.com>
2024-05-29 14:40:31 +0900

    factorybot>order_address_spec内の不要な記述を削除

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,8 +7,5 @@ FactoryBot.define do
     building_name { 'フロンティアビル3F' }
     tel_number { 11111111111 }
     token { 'tok_abcdefghijk12345678901234567' }
-
-    association :user_id
-    association :item_id
   end
 end
